38e59da496a650cc933504d29c7b907d0d096e16
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-09-07 00:23:05 +0300

    consumer

--- 03/demos/03_Writing-Functions/src/org/meroot/SomeMapping.java
+++ 03/demos/03_Writing-Functions/src/org/meroot/SomeMapping.java
@@ -18,13 +18,13 @@ public class SomeMapping {
 		
 		List<User> users = List.of(sarah, james, mary, john);
 		List<String> names = new ArrayList<>();
-		Function<User, String> toName = (User user) -> user.getName();
+		Function<User, String> toName = (User user) -> user.getName(); //userların get name ıle namelerı cekmesı saglıyor
 		for (User user : users) {
 			String name = toName.apply(user);
 			names.add(name);
 		}
 		users.forEach(user -> System.out.println(user));
-		Consumer<String> action = string -> System.out.println(string);
+		Consumer<String> action = string -> System.out.println(string); // consumerların aksıyonu alıuor
 		names.forEach(action);
 	}
 
