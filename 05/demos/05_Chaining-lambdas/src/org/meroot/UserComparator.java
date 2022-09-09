22f0144896c4857598175c184616f384e3458871
Mert kiziloglu <mert_besiktas24@hotmail.com>
2022-09-09 22:15:09 +0300

    user comparator

--- 05/demos/05_Chaining-lambdas/src/org/meroot/UserComparator.java
+++ 05/demos/05_Chaining-lambdas/src/org/meroot/UserComparator.java
@@ -15,9 +15,9 @@ public class UserComparator {
 		User mary = new User("Mary", 33);
 		User john1 = new User("John", 24);
 		User john2 = new User("John", 25);
-		
+		//kullanicilari kendi icinde karsilastiriyor
 		List<User> users = Arrays.asList(sarah, james, mary, john1, john2);
-		
+
 		Comparator<User> cmpName = Comparator.comparing(user -> user.getName());
 		Comparator<User> cmpAge = Comparator.comparing(user -> user.getAge());
 		Comparator<User> comparator = cmpName.thenComparing(cmpAge);
